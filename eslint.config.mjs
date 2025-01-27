4bcdc961c928937c8d8ce26bc6f9fc791cd0f413
Sanssolvant <zinedine.tomasella@hispeed.ch>
2025-01-27 23:27:12 +0100

    second commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,7 @@
 import mantine from 'eslint-config-mantine';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(...mantine, { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}'] });
+export default tseslint.config(...mantine, {
+  ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}'],
+  rules: { 'no-console': ['error', { allow: ['warn', 'error'] }] },
+});
